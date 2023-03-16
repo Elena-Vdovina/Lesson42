fbf2ef6398424fda9d823d3e5660749e2d38c359
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-03-16 23:56:32 +0100

    Add constructor

--- src/homework1/InvalidEmailExeption.java
+++ src/homework1/InvalidEmailExeption.java
@@ -4,4 +4,8 @@ package homework1;
 // При попытке создать Account нужно выбрасывать собственное исключение,
 // если в переданном адресе электронной почты нет символа '@'.
 public class InvalidEmailExeption extends IllegalArgumentException {
+
+  public InvalidEmailExeption(String email) {
+    super("Строка не является e-mail: " + email);
+  }
 }
