8b31fb66fa9cffdfbade4210b5e8684c68049091
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-03-17 10:36:04 +0100

    Read data and creat 3 object Reactangle

--- src/homework2/CompareRunner.java
+++ src/homework2/CompareRunner.java
@@ -1,11 +1,22 @@
 package homework2;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 // При попытке сравнить два прямоугольника они должны оказываться одинаковыми, если сами
 // прямоугольники как геометрические фигуры равны - если их можно совместить, повернув и наложив
 // друг на друга.
 public class CompareRunner {
-  public static void main(String[] args) {
-
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String l1 = br.readLine();
+    String w1 = br.readLine();
+    String l2 = br.readLine();
+    String w2 = br.readLine();
+    Rectangle r1 = new Rectangle(l1, w1);
+    Rectangle r2 = new Rectangle(l2, w2);
+    Rectangle r3 = new Rectangle(w2, l2);
   }
 
 }
